09f6e80e8a4fc4dbefbe1748d7a0c59cf7d2d43c
vandelvan <vandel_van@hotmail.com>
2020-05-18 17:54:16 -0500

    Los comentarios en MIPS son con # xd

--- InstructionSet/src/main/Conversor.java
+++ InstructionSet/src/main/Conversor.java
@@ -38,7 +38,7 @@ public class Conversor {
     //Funcion para ignorar los comentarios
     public String[] deleteComentarios(String[] lineas) {
         for (int i = 0; i < lineas.length; i++) {
-            lineas[i] = lineas[i].split(";")[0];    //Eliminamos comentarios de cada linea
+            lineas[i] = lineas[i].split("#")[0];    //Eliminamos comentarios de cada linea
             if(lineas[i].equals("")){   //si la linea sin comentario no tiene nada
                 System.arraycopy(lineas, i + 1, lineas, i, lineas.length - 1 - i);  //se elimina el espacio vacio del array
                 i--;    //volvemos a checar el indice ahora ocupado por otro string
